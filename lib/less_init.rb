44f02157f69d62088c79a9f342020a284fe57cc3
Kazuya Takeshima <mail@mitukiii.jp>
2012-02-04 13:59:44 +0900

    GifVJ initial commit.

---
+++ lib/less_init.rb
@@ -0,0 +1,19 @@
+module LessInitializer
+  def self.registered(app)
+# Enables support for Less template reloading for rack.
+# Store Less files by default within 'app/stylesheets/'
+# See http://github.com/kelredd/rack-less for more details.
+require 'rack/less'
+# optional - use as necessary
+Rack::Less.configure do |config|
+  config.compress = true
+  # other configs ...
+end
+app.use Rack::Less,
+:root      => app.root,
+:source    => 'stylesheets/',
+:public    => 'public/',
+:hosted_at => '/stylesheets'
+
+  end
+end
