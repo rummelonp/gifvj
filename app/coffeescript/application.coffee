13bf471906807e4fa3d6720c236d6026554e3a20
Kazuya Takeshima <mail@mitukiii.jp>
2012-02-07 05:16:49 +0900

    Make GifVJ JavaScript.

---
+++ app/coffeescript/application.coffee
@@ -0,0 +1,230 @@
+###
+# jQuery Extension
+###
+
+# Saved default show & hide
+$.fn.extend
+  __show: $.fn.show
+  __hide: $.fn.hide
+
+# Replace show & hide to bootstrap
+$.fn.extend
+  show: ->
+    this.addClass('show').removeClass('hide')
+  hide: ->
+    this.addClass('hide').removeClass('show')
+
+# Add methods to fn
+$.fn.extend
+  isInput: ->
+    this.is 'input, textarea, select'
+  enableElement: ->
+    this.attr 'disabled', null
+    this.unbind 'click.railsDisable'
+  disableElement: ->
+    this.attr 'disabled', 'disabled'
+    this.bind 'click.railsDisable', (e) ->
+      $.rails.stopEverything(e)
+  bindAjaxHandler: (handlers) ->
+    for eventName, handler of handlers
+      this.live 'ajax:' + eventName, handler
+    this
+
+# Add methods to Event
+$.extend $.Event.prototype, hasModifierKey: ->
+  return this.altKey || this.ctlrKey || this.metaKey || this.shiftKey
+
+###
+# GifJV Class
+###
+class GifVJ
+  constructor: (canvas, urls, handler) ->
+    @canvas = canvas
+    @context = @canvas.getContext '2d'
+    @urls = urls
+    @handler = handler || {}
+    @parsers = []
+    @datas = []
+    @errors = []
+    @parserHandler =
+      onParseProgress: $.proxy(@onParseProgress, this)
+      onParseComplete: $.proxy(@onParseComplete, this)
+      onParseError: $.proxy(@onParseError, this)
+    @load()
+
+  load: ->
+    self = this
+    for url in @urls
+      $.ajax
+        url: url
+        beforeSend: (req) ->
+          req.overrideMimeType 'text/plain; charset=x-user-defined'
+        complete: (req) ->
+          data = req.responseText
+          parser = new GifVJ.Parser data, null, self.parserHandler
+          self.parsers.push parser
+
+  playIfComplete: ->
+    return unless @datas.length + @errors.length == @parsers.length
+    @player = new GifVJ.Player(@canvas, @datas[0])
+    @handler.onComplete && @handler.onComplete this
+
+  onParseProgress: (parser) ->
+    return unless parser.frames.length > 0
+    frame = parser.frames[parser.frames.length - 1]
+    @canvas.width = parser.header.width
+    @canvas.height = parser.header.height
+    @context.putImageData(frame, 0, 0)
+
+  onParseComplete: (parser) ->
+    @datas.push
+      frames: parser.frames
+      width: parser.header.width
+      height: parser.header.height
+    percent = Math.round(@datas.length / @parsers.length * 100)
+    @handler.onProgress && @handler.onProgress this, percent
+    @playIfComplete()
+
+  onParseError: (parser, error) ->
+    @errors.push error
+    @handler.onError && @handler.onError this, error
+    @playIfComplete()
+
+class GifVJ.Parser
+  constructor: (data, canvas, handler) ->
+    @stream = new Stream(data)
+    @canvas = canvas || document.createElement 'canvas'
+    @handler = handler || {}
+    @frames = []
+    @parse()
+
+  parse: ->
+    try
+      parseGIF @stream,
+        hdr: $.proxy(@onParseHeader, this)
+        gce: $.proxy(@onParseGraphicControlExtension, this)
+        img: $.proxy(@onParseImage, this)
+        eof: $.proxy(@onEndOfFile, this)
+    catch error
+      if @handler.onParseError
+        @handler.onParseError this, error
+      else
+        throw error
+
+  pushFrame: ->
+    if @context
+      @frames.push @context.getImageData(0, 0, @header.width, @header.height)
+      @context = null
+
+  onParseHeader: (header) ->
+    @header = header
+    @canvas.width = @header.width
+    @canvas.height = @header.height
+
+  onParseGraphicControlExtension: (gce) ->
+    @pushFrame()
+    @transparency = if gce.transparencyGiven then gce.transparencyIndex else null
+    @disposal_method = gce.disposalMethod
+
+  onParseImage: (image) ->
+    unless @context
+      @context = @canvas.getContext '2d'
+    color_table = if image.lctFlag then image.lct else @header.gct
+    data = @context.getImageData(image.leftPos, image.topPos, image.width, image.height)
+    for pixel, index in image.pixels
+      if @transparency != pixel
+        data.data[index * 4 + 0] = color_table[pixel][0]
+        data.data[index * 4 + 1] = color_table[pixel][1]
+        data.data[index * 4 + 2] = color_table[pixel][2]
+        data.data[index * 4 + 3] = 255
+      else if @disposal_method == 2 || @disposal_method == 3
+        data.data[index * 4 + 3] = 0
+    @context.putImageData(data, image.leftPos, image.topPos)
+    @handler.onParseProgress && @handler.onParseProgress this
+
+  onEndOfFile: ->
+    @pushFrame()
+    @handler.onParseComplete && @handler.onParseComplete this
+
+class GifVJ.Player
+  constructor: (canvas, data)->
+    @canvas = canvas
+    @context = canvas.getContext('2d')
+    @setData data
+    @index = 0
+    @playing = false
+    @reverse = false
+    @delay = 100
+
+  setData: (data) ->
+    @data = data
+    @frames = @data.frames
+    @canvas.width = @data.width
+    @canvas.heifht = @data.height
+
+  setFrame: ->
+    frame = @frames[@index]
+    return unless frame
+    @context.putImageData(frame, 0, 0)
+
+  stepFrame: ->
+    return unless @playing
+    @setFrame()
+    if @reverse
+      @index -= 1
+      if @index < 0
+        @index = @frame.length - 1
+    else
+      @index += 1
+      if @index >= @frames.length
+        @index = 0
+    setTimeout($.proxy(@stepFrame, this), @delay)
+
+  play: ->
+    @playing = true
+    @stepFrame()
+
+  stop: ->
+    @playing = false
+
+  toggle: ->
+    if @playing
+      @stop()
+    else
+      @play()
+
+  nextFrame: ->
+    return if @playing
+    @index += 1
+    if @index >= @frames.length
+      @index = 0
+    @set()
+
+  prevFrame: ->
+    return if @playing
+    @index -= 1
+    if @index < 0
+      @index = @frames.length - 1
+    @set()
+
+  setReverse: (reverse) ->
+    @revecse = reverse
+
+  setDelay: (delay) ->
+    @delay = delay
+
+do ->
+  $('#form-gifs').bindAjaxHandler
+    beforeSend: ->
+      $(this).find('input').disableElement()
+    success: (req, data) ->
+      canvas = $('canvas').get(0)
+      progressbar = $('.progress').fadeIn().find('.bar')
+      new GifVJ canvas, data,
+        onProgress: (gifVj, percent) ->
+          progressbar.width(percent + '%')
+        onComplete: (gifVj) ->
+          $('#content').fadeOut()
+          gifVj.player.play()
+    complete: ->
+      $(this).find('input').enableElement()
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,312 @@
+/*
+# jQuery Extension
+*/
+var GifVJ;
+
+$.fn.extend({
+  __show: $.fn.show,
+  __hide: $.fn.hide
+});
+
+$.fn.extend({
+  show: function() {
+    return this.addClass('show').removeClass('hide');
+  },
+  hide: function() {
+    return this.addClass('hide').removeClass('show');
+  }
+});
+
+$.fn.extend({
+  isInput: function() {
+    return this.is('input, textarea, select');
+  },
+  enableElement: function() {
+    this.attr('disabled', null);
+    return this.unbind('click.railsDisable');
+  },
+  disableElement: function() {
+    this.attr('disabled', 'disabled');
+    return this.bind('click.railsDisable', function(e) {
+      return $.rails.stopEverything(e);
+    });
+  },
+  bindAjaxHandler: function(handlers) {
+    var eventName, handler;
+    for (eventName in handlers) {
+      handler = handlers[eventName];
+      this.live('ajax:' + eventName, handler);
+    }
+    return this;
+  }
+});
+
+$.extend($.Event.prototype, {
+  hasModifierKey: function() {
+    return this.altKey || this.ctlrKey || this.metaKey || this.shiftKey;
+  }
+  /*
+  # GifJV Class
+  */
+});
+
+GifVJ = (function() {
+
+  function GifVJ(canvas, urls, handler) {
+    this.canvas = canvas;
+    this.context = this.canvas.getContext('2d');
+    this.urls = urls;
+    this.handler = handler || {};
+    this.parsers = [];
+    this.datas = [];
+    this.errors = [];
+    this.parserHandler = {
+      onParseProgress: $.proxy(this.onParseProgress, this),
+      onParseComplete: $.proxy(this.onParseComplete, this),
+      onParseError: $.proxy(this.onParseError, this)
+    };
+    this.load();
+  }
+
+  GifVJ.prototype.load = function() {
+    var self, url, _i, _len, _ref, _results;
+    self = this;
+    _ref = this.urls;
+    _results = [];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      url = _ref[_i];
+      _results.push($.ajax({
+        url: url,
+        beforeSend: function(req) {
+          return req.overrideMimeType('text/plain; charset=x-user-defined');
+        },
+        complete: function(req) {
+          var data, parser;
+          data = req.responseText;
+          parser = new GifVJ.Parser(data, null, self.parserHandler);
+          return self.parsers.push(parser);
+        }
+      }));
+    }
+    return _results;
+  };
+
+  GifVJ.prototype.playIfComplete = function() {
+    if (this.datas.length + this.errors.length !== this.parsers.length) return;
+    this.player = new GifVJ.Player(this.canvas, this.datas[0]);
+    return this.handler.onComplete && this.handler.onComplete(this);
+  };
+
+  GifVJ.prototype.onParseProgress = function(parser) {
+    var frame;
+    if (!(parser.frames.length > 0)) return;
+    frame = parser.frames[parser.frames.length - 1];
+    this.canvas.width = parser.header.width;
+    this.canvas.height = parser.header.height;
+    return this.context.putImageData(frame, 0, 0);
+  };
+
+  GifVJ.prototype.onParseComplete = function(parser) {
+    var percent;
+    this.datas.push({
+      frames: parser.frames,
+      width: parser.header.width,
+      height: parser.header.height
+    });
+    percent = Math.round(this.datas.length / this.parsers.length * 100);
+    this.handler.onProgress && this.handler.onProgress(this, percent);
+    return this.playIfComplete();
+  };
+
+  GifVJ.prototype.onParseError = function(parser, error) {
+    this.errors.push(error);
+    this.handler.onError && this.handler.onError(this, error);
+    return this.playIfComplete();
+  };
+
+  return GifVJ;
+
+})();
+
+GifVJ.Parser = (function() {
+
+  function Parser(data, canvas, handler) {
+    this.stream = new Stream(data);
+    this.canvas = canvas || document.createElement('canvas');
+    this.handler = handler || {};
+    this.frames = [];
+    this.parse();
+  }
+
+  Parser.prototype.parse = function() {
+    try {
+      return parseGIF(this.stream, {
+        hdr: $.proxy(this.onParseHeader, this),
+        gce: $.proxy(this.onParseGraphicControlExtension, this),
+        img: $.proxy(this.onParseImage, this),
+        eof: $.proxy(this.onEndOfFile, this)
+      });
+    } catch (error) {
+      if (this.handler.onParseError) {
+        return this.handler.onParseError(this, error);
+      } else {
+        throw error;
+      }
+    }
+  };
+
+  Parser.prototype.pushFrame = function() {
+    if (this.context) {
+      this.frames.push(this.context.getImageData(0, 0, this.header.width, this.header.height));
+      return this.context = null;
+    }
+  };
+
+  Parser.prototype.onParseHeader = function(header) {
+    this.header = header;
+    this.canvas.width = this.header.width;
+    return this.canvas.height = this.header.height;
+  };
+
+  Parser.prototype.onParseGraphicControlExtension = function(gce) {
+    this.pushFrame();
+    this.transparency = gce.transparencyGiven ? gce.transparencyIndex : null;
+    return this.disposal_method = gce.disposalMethod;
+  };
+
+  Parser.prototype.onParseImage = function(image) {
+    var color_table, data, index, pixel, _len, _ref;
+    if (!this.context) this.context = this.canvas.getContext('2d');
+    color_table = image.lctFlag ? image.lct : this.header.gct;
+    data = this.context.getImageData(image.leftPos, image.topPos, image.width, image.height);
+    _ref = image.pixels;
+    for (index = 0, _len = _ref.length; index < _len; index++) {
+      pixel = _ref[index];
+      if (this.transparency !== pixel) {
+        data.data[index * 4 + 0] = color_table[pixel][0];
+        data.data[index * 4 + 1] = color_table[pixel][1];
+        data.data[index * 4 + 2] = color_table[pixel][2];
+        data.data[index * 4 + 3] = 255;
+      } else if (this.disposal_method === 2 || this.disposal_method === 3) {
+        data.data[index * 4 + 3] = 0;
+      }
+    }
+    this.context.putImageData(data, image.leftPos, image.topPos);
+    return this.handler.onParseProgress && this.handler.onParseProgress(this);
+  };
+
+  Parser.prototype.onEndOfFile = function() {
+    this.pushFrame();
+    return this.handler.onParseComplete && this.handler.onParseComplete(this);
+  };
+
+  return Parser;
+
+})();
+
+GifVJ.Player = (function() {
+
+  function Player(canvas, data) {
+    this.canvas = canvas;
+    this.context = canvas.getContext('2d');
+    this.setData(data);
+    this.index = 0;
+    this.playing = false;
+    this.reverse = false;
+    this.delay = 100;
+  }
+
+  Player.prototype.setData = function(data) {
+    this.data = data;
+    this.frames = this.data.frames;
+    this.canvas.width = this.data.width;
+    return this.canvas.heifht = this.data.height;
+  };
+
+  Player.prototype.setFrame = function() {
+    var frame;
+    frame = this.frames[this.index];
+    if (!frame) return;
+    return this.context.putImageData(frame, 0, 0);
+  };
+
+  Player.prototype.stepFrame = function() {
+    if (!this.playing) return;
+    this.setFrame();
+    if (this.reverse) {
+      this.index -= 1;
+      if (this.index < 0) this.index = this.frame.length - 1;
+    } else {
+      this.index += 1;
+      if (this.index >= this.frames.length) this.index = 0;
+    }
+    return setTimeout($.proxy(this.stepFrame, this), this.delay);
+  };
+
+  Player.prototype.play = function() {
+    this.playing = true;
+    return this.stepFrame();
+  };
+
+  Player.prototype.stop = function() {
+    return this.playing = false;
+  };
+
+  Player.prototype.toggle = function() {
+    if (this.playing) {
+      return this.stop();
+    } else {
+      return this.play();
+    }
+  };
+
+  Player.prototype.nextFrame = function() {
+    if (this.playing) return;
+    this.index += 1;
+    if (this.index >= this.frames.length) this.index = 0;
+    return this.set();
+  };
+
+  Player.prototype.prevFrame = function() {
+    if (this.playing) return;
+    this.index -= 1;
+    if (this.index < 0) this.index = this.frames.length - 1;
+    return this.set();
+  };
+
+  Player.prototype.setReverse = function(reverse) {
+    return this.revecse = reverse;
+  };
+
+  Player.prototype.setDelay = function(delay) {
+    return this.delay = delay;
+  };
+
+  return Player;
+
+})();
+
 (function() {
-
-
-
-}).call(this);
+  return $('#form-gifs').bindAjaxHandler({
+    beforeSend: function() {
+      return $(this).find('input').disableElement();
+    },
+    success: function(req, data) {
+      var canvas, progressbar;
+      canvas = $('canvas').get(0);
+      progressbar = $('.progress').fadeIn().find('.bar');
+      return new GifVJ(canvas, data, {
+        onProgress: function(gifVj, percent) {
+          return progressbar.width(percent + '%');
+        },
+        onComplete: function(gifVj) {
+          $('#content').fadeOut();
+          return gifVj.player.play();
+        }
+      });
+    },
+    complete: function() {
+      return $(this).find('input').enableElement();
+    }
+  });
+})();
