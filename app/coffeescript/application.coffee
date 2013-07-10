ae2e36be4c6c664f0d63116a91b0316204647017
Kazuya Takeshima <mail@mitukiii.jp>
2013-07-11 02:08:18 +0900

    Refactor application.coffee.

--- app/coffeescript/application.coffee
+++ app/coffeescript/application.coffee
@@ -1,60 +1,59 @@
-$ ->
+$window = $(window)
+$document = $(document)
+
+$document.ready ->
+  $container = $ '#canvas'
+  $canvas = $ 'canvas'
+  $form = $ '#form-gifs'
+  $progress = $ '.progress'
+  $bar = $progress.find '.bar'
+
+  resizeContainer = ->
+    $container.width $window.width()
+    $container.height $window.height()
+
+  gifVjHandler =
+    onProgress: (gifVj, percent) ->
+      $bar.width percent + '%'
+    onComplete: (gifVj) ->
+      $('nav, #content').fadeOut()
+      $canvas.removeClass 'prepared'
+      $document.keydown $.proxy gifVj.onKeyDown, gifVj
+      gifVj.start()
+
   startGifVJ = (data) ->
-    $w = $(window)
-    container = $('#canvas')
-    form = $('#form-gifs')
-    progress = $('.progress')
-    bar = progress.find('.bar')
-    canvas = $('canvas').addClass('prepared')
-
-    resizeContainer = ->
-      container.width $w.width()
-      container.height $w.height()
-
-    form.fadeOut ->
-      progress.fadeIn()
-
+    $canvas.addClass 'prepared'
+    $form.fadeOut ->
+      $progress.fadeIn()
     resizeContainer()
-    $w.resize resizeContainer
+    $window.resize resizeContainer
+    new GifVJ $canvas.get(0), data, gifVjHandler
 
-    setTimeout ->
-      new GifVJ canvas.get(0), data,
-        onProgress: (gifVj, percent) ->
-          bar.width(percent + '%')
-        onComplete: (gifVj) ->
-          setTimeout ->
-            $('nav').fadeOut()
-            $('#content').fadeOut()
-          , 0
-          canvas.removeClass('prepared')
-          $(document).keydown $.proxy(gifVj.onKeyDown, gifVj)
-          gifVj.start()
-    , 0
-
-  $('#form-gifs').bindAjaxHandler
+  $form.bindAjaxHandler
     beforeSend: ->
-      $(this).find('input').disableElement()
+      $form.find('input').disableElement()
     success: (req, data) ->
       startGifVJ(data)
     error: (e, req) ->
-      alert = $('#error-alert')
+      $alert = $('#error-alert')
         .text(req.responseText)
         .fadeIn()
       setTimeout ->
-        alert.fadeOut()
+        $alert.fadeOut()
       , 3000
     complete: ->
-      $(this).find('input').enableElement()
-  .find('input[type="text"]').keyup ->
-    text = $(this)
-    return if text.attr('disabled') == 'disabled'
-    submit = text.nextAll('input[type="submit"]')
-    if text.val().length > 0
-      submit.enableElement()
-    else
-      submit.disableElement()
+      $form.find('input').enableElement()
 
-  $(document).keypress (e) ->
+  $form.find('input[type="text"]').keyup ->
+    $text = $(this)
+    return if $text.attr('disabled') == 'disabled'
+    $submit = $text.nextAll('input[type="submit"]')
+    if $text.val().length > 0
+      $submit.enableElement()
+    else
+      $submit.disableElement()
+
+  $document.keypress (e) ->
     return if $(e.target).isInput()
     if e.which == 63 && e.shiftKey # ?
       $('#about').modal('toggle')
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,70 +1,72 @@
-$(function() {
-  var startGifVJ;
+var $document, $window;
+
+$window = $(window);
+
+$document = $(document);
+
+$document.ready(function() {
+  var $bar, $canvas, $container, $form, $progress, gifVjHandler, resizeContainer, startGifVJ;
+  $container = $('#canvas');
+  $canvas = $('canvas');
+  $form = $('#form-gifs');
+  $progress = $('.progress');
+  $bar = $progress.find('.bar');
+  resizeContainer = function() {
+    $container.width($window.width());
+    return $container.height($window.height());
+  };
+  gifVjHandler = {
+    onProgress: function(gifVj, percent) {
+      return $bar.width(percent + '%');
+    },
+    onComplete: function(gifVj) {
+      $('nav, #content').fadeOut();
+      $canvas.removeClass('prepared');
+      $document.keydown($.proxy(gifVj.onKeyDown, gifVj));
+      return gifVj.start();
+    }
+  };
   startGifVJ = function(data) {
-    var $w, bar, canvas, container, form, progress, resizeContainer;
-    $w = $(window);
-    container = $('#canvas');
-    form = $('#form-gifs');
-    progress = $('.progress');
-    bar = progress.find('.bar');
-    canvas = $('canvas').addClass('prepared');
-    resizeContainer = function() {
-      container.width($w.width());
-      return container.height($w.height());
-    };
-    form.fadeOut(function() {
-      return progress.fadeIn();
+    $canvas.addClass('prepared');
+    $form.fadeOut(function() {
+      return $progress.fadeIn();
     });
     resizeContainer();
-    $w.resize(resizeContainer);
-    return setTimeout(function() {
-      return new GifVJ(canvas.get(0), data, {
-        onProgress: function(gifVj, percent) {
-          return bar.width(percent + '%');
-        },
-        onComplete: function(gifVj) {
-          setTimeout(function() {
-            $('nav').fadeOut();
-            return $('#content').fadeOut();
-          }, 0);
-          canvas.removeClass('prepared');
-          $(document).keydown($.proxy(gifVj.onKeyDown, gifVj));
-          return gifVj.start();
-        }
-      });
-    }, 0);
+    $window.resize(resizeContainer);
+    return new GifVJ($canvas.get(0), data, gifVjHandler);
   };
-  $('#form-gifs').bindAjaxHandler({
+  $form.bindAjaxHandler({
     beforeSend: function() {
-      return $(this).find('input').disableElement();
+      return $form.find('input').disableElement();
     },
     success: function(req, data) {
       return startGifVJ(data);
     },
     error: function(e, req) {
-      var alert;
-      alert = $('#error-alert').text(req.responseText).fadeIn();
+      var $alert;
+      $alert = $('#error-alert').text(req.responseText).fadeIn();
       return setTimeout(function() {
-        return alert.fadeOut();
+        return $alert.fadeOut();
       }, 3000);
     },
     complete: function() {
-      return $(this).find('input').enableElement();
-    }
-  }).find('input[type="text"]').keyup(function() {
-    var submit, text;
-    text = $(this);
-    if (text.attr('disabled') === 'disabled') {
-      return;
-    }
-    submit = text.nextAll('input[type="submit"]');
-    if (text.val().length > 0) {
-      return submit.enableElement();
-    } else {
-      return submit.disableElement();
+      return $form.find('input').enableElement();
     }
   });
-  return $(document).keypress(function(e) {
+  $form.find('input[type="text"]').keyup(function() {
+    var $submit, $text;
+    $text = $(this);
+    if ($text.attr('disabled') === 'disabled') {
+      return;
+    }
+    $submit = $text.nextAll('input[type="submit"]');
+    if ($text.val().length > 0) {
+      return $submit.enableElement();
+    } else {
+      return $submit.disableElement();
+    }
+  });
+  return $document.keypress(function(e) {
     if ($(e.target).isInput()) {
       return;
     }
